37c820dae3a8ac566a48dfa8a2fd3f64e874c86f
¨Higginson¨ <higgins.matias@icloud.com>
2026-07-12 12:47:09 -0500

    Initial Commit

---
+++ learning c++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learning c++
+//
+//  Created by higginsonnn on 12/07/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
